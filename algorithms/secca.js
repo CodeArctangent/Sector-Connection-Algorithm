8da340bc1b98c97a0cf7a333e99a2f438ae66b74
Sir Code <taylorschneider07@yahoo.com>
2022-01-25 07:53:59 -0500

    Push updates to origin

--- algorithms/secca.js
+++ algorithms/secca.js
@@ -1,5 +1,40 @@
 class Secca {
-
+    static loadArray(strct, callback) {
+        let parts = {};
+        for (var y = 0; y < strct.length; ++y) {
+            for (var x = 0; x < strct[0].length; ++x) {
+                let part = {
+                    n:  false,
+                    ne: false,
+                    e:  false,
+                    se: false,
+                    s:  false,
+                    sw: false,
+                    w:  false,
+                    nw: false
+                };
+                if (callback(strct[y][x])) {
+                    if (callback(strct[y - 1][x]))
+                        part.n = true;
+                    if (callback(strct[y - 1][x + 1]))
+                        part.ne = true;
+                    if (callback(strct[y][x + 1]))
+                        part.e = true;
+                    if (callback(strct[y + 1][x + 1]))
+                        part.se = true;
+                    if (callback(strct[y + 1][x]))
+                        part.s = true;
+                    if (callback(strct[y + 1][x - 1]))
+                        part.sw = true;
+                    if (callback(strct[y][x - 1]))
+                        part.w = true;
+                    if (callback(strct[y - 1][x - 1]))
+                        part.w = true;
+                    parts[`${x}_${y}`] = part;
+                }
+            }
+        }
+    }
 }
 
 module.exports = {
--- webview/preload.js
+++ webview/preload.js
@@ -18,6 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
     canvas.width = config.simulation.width;
     canvas.height = config.simulation.height;
     img = ctx.createImageData(canvas.width, canvas.height);
+    drawGrid();
+    drawStructure();
 });
 
 const dir = (n) => Math.round(n) >= 0 ? 1 : -1;
@@ -44,6 +46,46 @@ function makeButtonsUsable() {
     document.querySelector('.tb-close').addEventListener('click', () => {
         window.close();
     });
+}
+
+let cellCount = 10;
+let margin = 3;
+
+function drawGrid() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    let gx = canvas.width / cellCount;
+    let gy = canvas.height / cellCount;
+    for (let y = 0; y < cellCount; ++y) {
+        for (let x = 0; x < cellCount; ++x) {
+            ctx.fillStyle = 'white';
+            ctx.fillRect(gx * (x + 1), gy * y, 1, gy);
+            ctx.fillRect(gx * x, gy * (y + 1), gx, 1);
+        }
+    }
+}
+
+let originStructure = [
+    [0, 0, 1, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 1],
+    [1, 0, 1, 0]
+];
+let position = [3, 3];
+
+function drawStructure() {
+    let gx = canvas.width / cellCount;
+    let gy = canvas.height / cellCount;
+    let width = originStructure[0].length;
+    let height = originStructure.length;
+    let objects = Secca.loadArray(originStructure, (val) => val == 1 ? true : false);
+    for (let y = 0; y < height; ++y) {
+        for (let x = 0; x < width; ++x) {
+            if (originStructure[y][x] == 1) {
+                ctx.fillRect(gx * (x + position[0]) + margin, gy * (y + position[1]) + margin, gx - margin * 2, gy - margin * 2);
+            }
+        }
+    } 
 }
 
 console.log(seccaIsLoaded);
